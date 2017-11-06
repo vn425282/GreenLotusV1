fe38c746e39cb5aaa8952e68ac491e78e59c6724
TriNguyen <vn425282>
2017-11-06 08:47:50 +0700

    - update routing

--- client_frontend/GreenLotusClient/src/app/app.routing.ts
+++ client_frontend/GreenLotusClient/src/app/app.routing.ts
@@ -2,8 +2,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pages', pathMatch: 'full' },
   { path: 'pages', loadChildren: './pages/pages.module#PagesModule'},
-  { path: '', redirectTo: 'pages', pathMatch: 'full' }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes, { useHash: false });
