a1e725a5ebfbcd2c2b1c9b7985c4ed001588e66d
TriNguyen <vn425282>
2017-11-06 10:13:57 +0700

    - update

--- client_frontend/GreenLotusClient/src/app/services/shared/shared.service.ts
+++ client_frontend/GreenLotusClient/src/app/services/shared/shared.service.ts
@@ -5,7 +5,7 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class SharedService {
-    public baseURL = 'http://10.211.55.3:8088/';
+    public baseURL = 'http://greenlotus.vn/api/';
     public headers = new Headers({ 'Content-Type': 'application/json' });
     public options = new RequestOptions({ headers: this.headers });
     public flagLogin: any = localStorage.getItem('login');
